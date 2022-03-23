c2dc191691f4189621a4481f40c37f83501d2b45
JulFruit <julienjeanjacquot@gmail.com>
2022-03-23 10:41:34 +0100

    tt

--- server.js
+++ server.js
@@ -12,8 +12,18 @@ const server = express()
 
 const io = socketIO(server);
 
+var messages = [];
+
 io.on('connection', function (socket) {
-  socket.emit('faitUneAlerte');
+  // On donne la liste des messages (événement créé du côté client)
+  socket.emit('recupererMessages', messages);
+  // Quand on reçoit un nouveau message
+  socket.on('nouveauMessage', function (mess) {
+      // On l'ajoute au tableau (variable globale commune à tous les clients connectés au serveur)
+      messages.push(mess);
+      // On envoie à tous les clients connectés (sauf celui qui a appelé l'événement) le nouveau message
+      socket.broadcast.emit('recupererNouveauMessage', mess);
+  });
 });
 
 
