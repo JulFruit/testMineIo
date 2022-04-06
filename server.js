e346be19a922fe12ce79f677ca1c6afadc00cb2a
JulFruit <julienjeanjacquot@gmail.com>
2022-04-06 11:47:44 +0200

    ee

--- server.js
+++ server.js
@@ -12,57 +12,18 @@ const server = express()
 
 const io = socketIO(server);
 
-var players = {};
-foods = [[2,7],[2,0],[0,5]]
+var messages = [];
 
 io.on('connection', function (socket) {
-  //permet de creer un nouveau joueur
-	socket.on('newPlayer', function (pseudo) {
-		players[pseudo] = {"position":[0,0],"size":4};
-		console.log(players);
-	});
-
-	// On donne les donnée joeurs
-	socket.emit('recupererInfos', players);
-	// On donne les foods
-	socket.emit('recupererFoods', foods);
-
-	// Quand on reçoit une nouvelle coo
-	socket.on('newPacket', function (packet) {
-		//update position
-		console.log(packet);
-		players[packet["name"]]["position"][0] += packet["direction"][0]
-		players[packet["name"]]["position"][1] += packet["direction"][1]
-
-		//check for food
-		foods.forEach(e =>{
-			if ((Math.sqrt((e[0]-players[packet["name"]]["position"][0])**2 + (e[1]-players[packet["name"]]["position"][1])**2)) > players[packet["name"]]["size"]){
-				foods.pop(e)
-				players[packet["name"]]["size"] += 2
-				foods.push([Math.round(Math.random()*10),Math.round(Math.random()*10)])
-			}
-		})
-
-		//check for collision
-		for (player in players){
-			if (player != packet["name"]){
-				if ((Math.sqrt((players[player]["position"][0]-players[packet["name"]]["position"][0])**2 + (players[player]["position"][1]-players[packet["name"]]["position"][1])**2)) < players[packet["name"]]["size"]){
-					if (players[packet["name"]]["size"] > players[player]["size"]){
-						players[packet["name"]]["size"] += players[player]["size"]
-						delete players[player]
-					}else{
-						players[player]["size"] += players[packet["name"]]["size"]
-						delete players[packet["name"]]
-					}
-				}
-			}
-		}
-
-		console.log(foods)
-		// On envoie à tous les clients connectés 
-		socket.emit('recupererInfos', players);
-		socket.emit('recupererFoods', foods);
-	});
+  // On donne la liste des messages (événement créé du côté client)
+  socket.emit('recupererMessages', messages);
+  // Quand on reçoit un nouveau message
+  socket.on('nouveauMessage', function (mess) {
+      // On l'ajoute au tableau (variable globale commune à tous les clients connectés au serveur)
+      messages.push(mess);
+      // On envoie à tous les clients connectés (sauf celui qui a appelé l'événement) le nouveau message
+      socket.broadcast.emit('recupererNouveauMessage', mess);
+  });
 });
 
 
