10e2a4fdb4ffaeb66a5187d54da6c3761b1b485f
JulFruit <julienjeanjacquot@gmail.com>
2022-04-13 10:51:37 +0200

    aa

--- server.js
+++ server.js
@@ -1,69 +1,98 @@
-'use strict';
+var http    =	require('http');
+var fs      =	require('fs');
 
-const express = require('express');
-const socketIO = require('socket.io');
+// Creation du serveur
+var app = http.createServer(function (req, res) {
+	// On lit notre fichier app.html
+	var t_req =req.url.split('?')[0]
+	if (t_req==='/'){
+	fs.readFile('./Connexion.html', 'utf-8', function(error, content) {
+		res.writeHead(200, {'Content-Type' : 'text/html'});
+		res.end(content);
+	});
+}
+if (t_req==='/test'){
+	console.log(players)
+	fs.readFile('./index.html', 'utf-8', function(error, content) {
+		res.writeHead(200, {'Content-Type' : 'text/html'});
+		res.end(content);
+})};	
+});
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-  .get('/', (req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .get('/aa', (req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
+// Variables globales
+// Ces variables resteront durant toute la vie du seveur pour et sont commune pour chaque client (node server.js)
+// liste des messages de la forme { pseudo :{ coos:{x:1 , y:2}, size : 231}}
 var players = {};
-var foods = [[2,7],[2,0],[0,5]];
+foods = [[2,7],[2,0],[0,5]]
+//// SOCKET.IO ////
 
-io.on('connection', function (socket) {
-  //permet de creer un nouveau joueur
+var io = require('socket.io')(http);
+
+// Socket io ecoute maintenant notre application !
+io = io.listen(app); 
+
+
+// Quand une personne se connecte au serveur
+io.sockets.on('connection', function (socket) {
+
+	//permet de creer un nouveau joueur
 	socket.on('newPlayer', function (pseudo) {
 		players[pseudo] = {"position":[0,0],"size":4};
 		console.log(players);
 	});
-  	// On donne les donnée joeurs
+
+	// On donne les donnée joeurs
 	socket.emit('recupererInfos', players);
 	// On donne les foods
 	socket.emit('recupererFoods', foods);
 
-  // Quand on reçoit une nouvelle coo
+	// login
+	socket.on('Credential', function (cred){
+		console.log(cred.pseudo)
+		console.log(cred.color)
+	});
+	// Quand on reçoit une nouvelle coo
 	socket.on('newPacket', function (packet) {
 		//update position
 		console.log(packet);
-		players[packet["name"]]["position"][0] += packet["direction"][0];
-		players[packet["name"]]["position"][1] += packet["direction"][1];
+		players[packet["name"]]["position"][0] += packet["direction"][0]
+		players[packet["name"]]["position"][1] += packet["direction"][1]
 
 		//check for food
 		foods.forEach(e =>{
 			if ((Math.sqrt((e[0]-players[packet["name"]]["position"][0])**2 + (e[1]-players[packet["name"]]["position"][1])**2)) > players[packet["name"]]["size"]){
-				foods.pop(e);
-				players[packet["name"]]["size"] += 2;
+				foods.pop(e)
+				players[packet["name"]]["size"] += 2
 				foods.push([Math.round(Math.random()*10),Math.round(Math.random()*10)])
 			}
 		})
 
 		//check for collision
-		for (var player in players){
+		for (player in players){
 			if (player != packet["name"]){
 				if ((Math.sqrt((players[player]["position"][0]-players[packet["name"]]["position"][0])**2 + (players[player]["position"][1]-players[packet["name"]]["position"][1])**2)) < players[packet["name"]]["size"]){
 					if (players[packet["name"]]["size"] > players[player]["size"]){
-						players[packet["name"]]["size"] += players[player]["size"];
-						delete players[player];
+						players[packet["name"]]["size"] += players[player]["size"]
+						delete players[player]
 					}else{
-						players[player]["size"] += players[packet["name"]]["size"];
-						delete players[packet["name"]];
+						players[player]["size"] += players[packet["name"]]["size"]
+						delete players[packet["name"]]
 					}
 				}
 			}
 		}
 
-		console.log(foods);
+		console.log(foods)
 		// On envoie à tous les clients connectés 
 		socket.emit('recupererInfos', players);
 		socket.emit('recupererFoods', foods);
 	});
+
+
 });
 
+///////////////////
 
-
+// Notre application ecoute sur le port 8080
+app.listen(3000);
+console.log('Live Chat App running at http://localhost:8080/');
