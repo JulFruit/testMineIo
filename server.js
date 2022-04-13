dc6002db4754700064e4ef2cc6d7984c0be324c4
JulFruit <julienjeanjacquot@gmail.com>
2022-04-13 11:21:03 +0200

    new br

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-	.get('/', (req, res) => res.sendFile(INDEX, { root: __dirname }))
-	.get('/aa', (req, res) => res.sendFile(INDEX, { root: __dirname }))
-	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
