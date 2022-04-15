61ceb34d2d1555063a2afd374857d856f41509dd
JulFruit <julienjeanjacquot@gmail.com>
2022-04-15 13:53:13 +0200

    zz

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
+  .use(('/', express.static('/')))
   .get('/', (req, res) => res.sendFile('/Connexion.html', { root: __dirname }))
-  .get('/test', (req, res) => res.sendFile('/game.html', { root: __dirname }))
+  .get('/test', (req, res) => res.sendFile('/index.html', { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
