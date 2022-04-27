bc1ae0abb62b754efd20e1c5f248d331267d1c16
JulFruit <julienjeanjacquot@gmail.com>
2022-04-27 15:50:56 +0200

    eee

--- local.io/server.js
+++ local.io/server.js
@@ -10,7 +10,7 @@ const server = express()
   .use(express.static(__dirname))
   .get('/', (req, res) => res.sendFile('/Connexion.html', { root: __dirname }))
   .get('/test', (req, res) => res.sendFile('/Game.html', { root: __dirname }))
-  .get('/death', (req, res) => res.sendFile('/Gameover.html', { root: __dirname }))
+  .get('/death', (req, res) => res.sendFile('/GameOver.html', { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
@@ -19,10 +19,10 @@ var players = {};
 var foods = [[5,5],[5,-5]];
 
 let size = 200;
-// for(let i=0; i<10; i++){
-// 	console.log(i);
-// 	foods.push([Math.floor(Math.random()*size-size/2),Math.floor(Math.random()*size-size/2)]);
-// }
+for(let i=0; i<100; i++){
+	console.log(i);
+	foods.push([Math.floor(Math.random()*size-size/2),Math.floor(Math.random()*size-size/2)]);
+}
 console.log(foods);
 const vitesse = (size)=>{}
 
@@ -50,17 +50,21 @@ io.on('connection', function (socket) {
 		var prev_time=players[packet["name"]]["time"]
 		players[packet["name"]]["time"]=new Date()
 		var delta_t=players[packet["name"]]["time"]-prev_time
-		console.log(packet);
-		console.log(delta_t);
+		//console.log(packet);
+		//console.log(delta_t);
 		// players[packet["name"]]["position"][0] += packet["direction"][0] * 10/players[packet['name']]["size"];
 		// players[packet["name"]]["position"][1] += packet["direction"][1] * 10/players[packet['name']]["size"];
-		players[packet["name"]]["position"][0] += packet["direction"][0] * 10*delta_t/1000;
-		players[packet["name"]]["position"][1] += packet["direction"][1] * 10*delta_t/1000;
-		//console.log(players);
+		players[packet["name"]]["position"][0] += packet["direction"][0] * 1*delta_t/1000;
+		players[packet["name"]]["position"][1] += packet["direction"][1] * 1*delta_t/1000;
+		console.log(players);
 		//check for food
 		foods.forEach(e =>{
+			console.log(e);
+			console.log(players[packet["name"]]["position"])
+			console.log("zzzz")
 			if ((Math.sqrt((e[0]-players[packet["name"]]["position"][0])**2 + (e[1]-players[packet["name"]]["position"][1])**2)) < players[packet["name"]]["size"]/2){
-				foods.pop(e);
+				console.log("aaa")
+				foods = foods.filter(function(f) { return f !== e })
 				foods.push([Math.floor(Math.random()*size-size/2),Math.floor(Math.random()*size-size/2)]);
 				players[packet["name"]]["size"] += 1;
 				
