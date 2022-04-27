ee2357dead29950f6ccf07bfc6b5e4f8072701c1
Ikaroc-info <leo.bertrand@depinfonancy.net>
2022-04-27 13:08:56 +0200

    local

--- local.io/server.js
+++ local.io/server.js
@@ -10,6 +10,7 @@ const server = express()
   .use(express.static(__dirname))
   .get('/', (req, res) => res.sendFile('/Connexion.html', { root: __dirname }))
   .get('/test', (req, res) => res.sendFile('/Game.html', { root: __dirname }))
+  .get('/death', (req, res) => res.sendFile('/Gameover.html', { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
@@ -28,7 +29,7 @@ const vitesse = (size)=>{}
 io.on('connection', function (socket) {
   //permet de creer un nouveau joueur
 	socket.on('newPlayer', function (infPlayer) {
-		players[infPlayer['name']] = {"position":[0,0],"size":4,"color":infPlayer['color']};
+		players[infPlayer['name']] = {"position":[0,0],"size":4,"color":infPlayer['color'],"time":new Date()};
 		console.log(players);
 	});
   	// On donne les donnée joeurs
@@ -46,10 +47,15 @@ io.on('connection', function (socket) {
 	socket.on('newPacket', function (packet) {
 		//update position
 		console.log(foods)
-		// console.log(packet);
-		console.log(players);
-		players[packet["name"]]["position"][0] += packet["direction"][0] * 0.01/players[packet['name']]["size"];
-		players[packet["name"]]["position"][1] += packet["direction"][1] * 0.01/players[packet['name']]["size"];
+		var prev_time=players[packet["name"]]["time"]
+		players[packet["name"]]["time"]=new Date()
+		var delta_t=players[packet["name"]]["time"]-prev_time
+		console.log(packet);
+		console.log(delta_t);
+		// players[packet["name"]]["position"][0] += packet["direction"][0] * 10/players[packet['name']]["size"];
+		// players[packet["name"]]["position"][1] += packet["direction"][1] * 10/players[packet['name']]["size"];
+		players[packet["name"]]["position"][0] += packet["direction"][0] * 10*delta_t/1000;
+		players[packet["name"]]["position"][1] += packet["direction"][1] * 10*delta_t/1000;
 		//console.log(players);
 		//check for food
 		foods.forEach(e =>{
@@ -64,15 +70,13 @@ io.on('connection', function (socket) {
 		//check for collision
 		for (var player in players){
 			if (player != packet["name"]){
-				if ((Math.sqrt((players[player]["position"][0]-players[packet["name"]]["position"][0])**2 + (players[player]["position"][1]-players[packet["name"]]["position"][1])**2)) < (players[packet["name"]]["size"]/2 + players[player]["size"]/2)){
-					if (players[packet["name"]]["size"] > players[player]["size"]){
-						console.log(players[player] + "is dead")
-						//players[packet["name"]]["size"] += players[player]["size"];
-						//delete players[player];
-					}else{
-						//players[player]["size"] += players[packet["name"]]["size"];
-						//delete players[packet["name"]];
+				if ((Math.sqrt((players[player]["position"][0]-players[packet["name"]]["position"][0])**2 + (players[player]["position"][1]-players[packet["name"]]["position"][1])**2)) < players[packet["name"]]["size"]){
+					if (players[packet["name"]]["size"] <= players[player]["size"]){
+						players[player]["size"]+=players[packet["name"]]["size"];
+						socket.emit('death',"req");
+						delete players[packet["name"]];
 					}
+				}
 				}
 			}
 		}
